89006af455ee454fe56a47da083a9d393416051a
minhtu <leminhtu17062003@gmail.com>
2024-02-27 22:17:50 +0700

    initial commit

--- src/views/App.js
+++ src/views/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button>
+          Hello World!
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
